b4c4920919844899652edeae8e3480eab6962d82
imichen <a2233107@gmail.com>
2016-05-27 17:20:25 +0800

    target_spec

---
+++ lifeeditor/src/main/java/com/lifeeditor/service/TargetSpecService.java
@@ -0,0 +1,21 @@
+package com.lifeeditor.service;
+
+import java.util.List;
+
+import com.lifeeditor.model.target_spec.*;
+
+public class TargetSpecService {
+	private Target_specDAO_interface dao = null;
+	
+	public TargetSpecService() {
+		this.dao = new Target_specJDBCDAO();
+	}
+	
+	public List<Target_specVO> getAll() {
+		return dao.getAll();
+	}
+	
+//	public List<Target_specVO> getAllByHotMan() {
+//		return dao.getAllByHotMan();
+//	}
+}
