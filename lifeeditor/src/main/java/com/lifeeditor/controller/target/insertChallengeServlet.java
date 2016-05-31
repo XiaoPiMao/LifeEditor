c1f06a02b0fcd6b907004f93a08e183ee6aa8759
jones <jonesandjay123@yahoo.com.tw>
2016-05-31 18:01:28 +0800

    成就系統新增許多功能，用Jquery~

--- lifeeditor/src/main/java/com/lifeeditor/controller/target/insertChallengeServlet.java
+++ lifeeditor/src/main/java/com/lifeeditor/controller/target/insertChallengeServlet.java
@@ -1,15 +1,23 @@
 package com.lifeeditor.controller.target;
 
 import java.io.IOException;
-import java.util.LinkedList;
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.util.List;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import com.lifeeditor.model.achievement.AchievementVO;
+import com.lifeeditor.model.target.TargetVO;
+import com.lifeeditor.service.AchievementService;
+import com.lifeeditor.service.TargetService;
+
+@WebServlet("/ChallengeServlet")
 public class insertChallengeServlet extends HttpServlet{
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
@@ -22,12 +30,17 @@ public class insertChallengeServlet extends HttpServlet{
 			throws ServletException, IOException {
 
 		req.setCharacterEncoding("UTF-8");
-		String action = req.getParameter("action");
+		String keyword = req.getParameter("keyword");
 		
-
-
-
+		TargetService trgSrvc= new TargetService();
+		List<TargetVO> list = trgSrvc.findKey(keyword);
 		
+		Gson gson = new Gson();
+		String jsonStr = gson.toJson(list);
+		res.setCharacterEncoding("UTF-8");
+		PrintWriter os = res.getWriter();
+		os.print(jsonStr);
+			
 	}
 	
 }
---
+++ lifeeditor/src/main/java/com/lifeeditor/service/TargetService.java
@@ -0,0 +1,87 @@
+package com.lifeeditor.service;
+
+import java.sql.Blob;
+import java.sql.Date;
+import java.util.List;
+
+import com.lifeeditor.model.achievement.AchievementVO;
+import com.lifeeditor.model.target.*;
+
+public class TargetService {
+
+	private TargetDAO_interface dao;
+	
+	public TargetService (){
+		dao = new TargetDAO_JNDI();
+	}
+	
+	public TargetVO addTrg(String trgName,Integer typeID,Integer sectionID,Integer difficulty,
+			String intention,Integer privacy,Integer genkiBar,Integer achID,Integer priority,
+			Integer remindTimes,Integer trgType,Integer punishment,Integer status,Date timeStart,Date timeFinish,Date doneTime){
+	
+		TargetVO TrgVO = new TargetVO();
+		TrgVO.setTrgName(trgName);
+		TrgVO.setTypeID(typeID);
+		TrgVO.setSectionID(sectionID);
+		TrgVO.setDifficulty(difficulty);
+		TrgVO.setIntention(intention);
+		TrgVO.setPrivacy(privacy);
+		TrgVO.setGenkiBar(genkiBar);
+		TrgVO.setAchID(achID);
+		TrgVO.setPriority(priority);
+		TrgVO.setRemindTimes(remindTimes);
+		TrgVO.setTrgType(trgType);
+		TrgVO.setPunishment(punishment);
+		TrgVO.setStatus(status);
+		TrgVO.setTimeStart(timeStart);
+		TrgVO.setTimeFinish(timeFinish);
+		TrgVO.setDoneTime(doneTime);
+		dao.insert(TrgVO);	
+		return TrgVO;
+	}
+	
+	
+	public TargetVO updateTrg(Integer targetID,String trgName,Integer typeID,Integer sectionID,Integer difficulty,
+			String intention,Integer privacy,Integer genkiBar,Integer achID,Integer priority,
+			Integer remindTimes,Integer trgType,Integer punishment,Integer status,Date timeStart,Date timeFinish,Date doneTime){
+		
+		TargetVO TrgVO = new TargetVO();
+		TrgVO.setTargetID(targetID);
+		TrgVO.setTrgName(trgName);
+		TrgVO.setTypeID(typeID);
+		TrgVO.setSectionID(sectionID);
+		TrgVO.setDifficulty(difficulty);
+		TrgVO.setIntention(intention);
+		TrgVO.setPrivacy(privacy);
+		TrgVO.setGenkiBar(genkiBar);
+		TrgVO.setAchID(achID);
+		TrgVO.setPriority(priority);
+		TrgVO.setRemindTimes(remindTimes);
+		TrgVO.setTrgType(trgType);
+		TrgVO.setPunishment(punishment);
+		TrgVO.setStatus(status);
+		TrgVO.setTimeStart(timeStart);
+		TrgVO.setTimeFinish(timeFinish);
+		TrgVO.setDoneTime(doneTime);
+		dao.update(TrgVO);
+		return dao.findByPrimaryKey(targetID);
+	}
+	
+	public void deleteTrg(Integer targetID) {
+		dao.delete(targetID);
+	}
+
+	public TargetVO getOneTrg(Integer targetID) {
+		return dao.findByPrimaryKey(targetID);
+	}
+
+	public List<TargetVO> getAll() {
+		return dao.getAll();
+	}
+
+	public List<TargetVO> findKey(String keyword) {
+		return dao.findByKeyword(keyword);
+	}
+
+	
+}
