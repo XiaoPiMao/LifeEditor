9a1fc64e3175866ddce22424a8a22d64eb4891c2
da <sa123456>
2016-06-08 10:00:02 +0800

    sql 微修

--- lifeeditor/src/main/resources/InitialTable.sql
+++ lifeeditor/src/main/resources/InitialTable.sql
@@ -95,7 +95,7 @@ CREATE TABLE achievement(
 );
 
 CREATE TABLE ach_list(
-	ach_list int IDENTITY PRIMARY KEY,
+	ach_listID int IDENTITY PRIMARY KEY,
 	userID int FOREIGN KEY REFERENCES user_spec(userID) NOT NULL,
 	achID int FOREIGN KEY REFERENCES achievement(achID) NOT NULL,
 );
