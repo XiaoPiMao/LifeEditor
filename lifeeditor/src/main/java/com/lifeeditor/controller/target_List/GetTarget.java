1cc4f247def41ed74292ff73a66bc4ebe95b9314
Fled <a391626@gmail.com>
2016-06-13 13:40:25 +0800

    小修

--- lifeeditor/src/main/java/com/lifeeditor/controller/target_List/GetTarget.java
+++ lifeeditor/src/main/java/com/lifeeditor/controller/target_List/GetTarget.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.lifeeditor.model.target_list.Target_ListVO;
 import com.lifeeditor.model.target_spec.Target_specVO;
 import com.lifeeditor.model.user_spec.user_specVO;
@@ -23,11 +24,11 @@ import com.lifeeditor.utility.MyGson;
  * Servlet implementation class getTarget
  */
 @WebServlet("/getTarget")
-public class getTarget extends HttpServlet {
+public class GetTarget extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 
-    public getTarget() {
+    public GetTarget() {
         super();
         
     }
@@ -43,9 +44,12 @@ public class getTarget extends HttpServlet {
 		user_specVO user = (user_specVO)request.getSession().getAttribute("LoginOK");
 		List<Target_ListVO> list = trgSvc.findByUserID(user.getUserID());
 		JsonArray jsonArray = new JsonArray();
-		Gson gson = new Gson();
-		for(Target_ListVO targetList : list) {
-			jsonArray.add(gson.toJsonTree(targetList.getTrgVO()));
+		JsonObject jsonObject = null;
+		Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+		for(Target_ListVO trgList : list ) {
+			jsonObject = new JsonObject();
+			jsonObject.add(trgList.getTargetListID().toString(), gson.toJsonTree(trgList.getTrgVO()));
+			jsonArray.add(jsonObject);
 		}
 		
 		response.setContentType("application/json");
