538772569ef07f8b70c38300b29dcfe9f8f035c3
Fled <a391626@gmail.com>
2016-06-14 13:34:00 +0800

    抓個人的target

--- lifeeditor/src/main/java/com/lifeeditor/controller/target_List/GetJTargetByUser.java
+++ lifeeditor/src/main/java/com/lifeeditor/controller/target_List/GetJTargetByUser.java
@@ -2,6 +2,8 @@ package com.lifeeditor.controller.target_List;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.*;
-
+import com.lifeeditor.model.target.TargetVO;
 import com.lifeeditor.model.target_list.Target_ListVO;
 import com.lifeeditor.model.user_spec.user_specVO;
 import com.lifeeditor.service.Target_List_Service;
@@ -31,11 +33,20 @@ public class GetJTargetByUser extends HttpServlet {
 		user_specVO user = (user_specVO)request.getSession().getAttribute("LoginOK");
 		Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().setDateFormat("yyyy-MM-dd").create();
 		List<Target_ListVO> list = trgListSvc.findByUserID(user.getUserID());
-		JsonObject jsonObj = null;
 		JsonArray jsonArray = new JsonArray();
+		JsonObject jsonObj = null;
 		for(Target_ListVO trgList : list) {
 			jsonObj = new JsonObject();
-			jsonObj.add(trgList.getTargetListID().toString(), gson.toJsonTree(trgList.getTrgVO()));
+			TargetVO trg = trgList.getTrgVO();
+			JsonObject JTrg = gson.toJsonTree(trg).getAsJsonObject();
+			jsonObj.addProperty("typeID", trg.getTypeVO().getTypeID());
+			jsonObj.addProperty("secID", trg.getSectionVO().getSecID());
+			
+			for(Entry<String,JsonElement> entry : JTrg.entrySet()) {
+				String key = entry.getKey();
+				if(!key.equals("typeVO") && !key.equals("sectionVO")) 
+					jsonObj.add(key, entry.getValue());
+			}
 			jsonArray.add(jsonObj);
 		}
 		
