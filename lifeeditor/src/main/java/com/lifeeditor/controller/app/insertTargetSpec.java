a6ba7952c74445227c19ef70e5b5ad48aca8c144
Silver <akyo1313@gmail.com>
2016-06-20 19:47:36 +0800

    完成APP

--- lifeeditor/src/main/java/com/lifeeditor/controller/app/insertTargetSpec.java
+++ lifeeditor/src/main/java/com/lifeeditor/controller/app/insertTargetSpec.java
@@ -3,12 +3,27 @@ package com.lifeeditor.controller.app;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.sql.Blob;
+import java.util.Base64;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import javax.sql.rowset.serial.SerialBlob;
+
+import com.lifeeditor.model.target.TargetVO;
+import com.lifeeditor.model.target_list.Target_ListVO;
+import com.lifeeditor.model.target_spec.Target_specVO;
+import com.lifeeditor.model.user_spec.user_specVO;
+import com.lifeeditor.service.TargetSpecService;
+import com.lifeeditor.service.Target_List_Service;
+import com.lifeeditor.utility.DoBase64;
 
 import Decoder.BASE64Decoder;
 
@@ -32,24 +47,65 @@ public class insertTargetSpec extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		 	request.setCharacterEncoding("utf-8");  
 		 	response.setCharacterEncoding("utf-8");  
-		     response.setContentType("text/html");  
-		     String photo=request.getParameter("photo");  
-		     String name=request.getParameter("name");  
-		     System.out.println(photo);  
-		    //对发送来的Base64数据进行解码。生成字节数组  
-		      byte [] photos = new BASE64Decoder().decodeBuffer(photo);  
-		      for (int i=0;i<photos.length;i++){  
-		          if(photos[i]<0){  
-		              photos[i]+=256;  
-		          }  
-		      }  
-		      //F  
-		      File file=new File("d:","decode.png");  
-		      File filename=new File("d:\\name.txt");  
-		      if(!file.exists()){file.createNewFile();}if(!filename.exists()){filename.createNewFile();}  
-		      FileOutputStream out=new FileOutputStream(file);  
-		      FileOutputStream out1=new FileOutputStream(filename);  
-		      out.write(photos);out.flush();out.close();  
+		     response.setContentType("text/html;charset=UTF-8");  
+		     
+		      try {
+		    	  String photo=request.getParameter("photo");  
+				     String note=request.getParameter("note");
+				     String targetID=request.getParameter("targetID");
+				     Integer tagID = Integer.parseInt(targetID);
+				   
+				   
+				     System.out.println(photo);  
+				   
+				     
+				     byte [] photos = Base64.getMimeDecoder().decode(photo.getBytes(StandardCharsets.UTF_8));
+
+				      for (int i=0;i<photos.length;i++){  
+				          if(photos[i]<0){  
+				              photos[i]+=256;  
+				          }  
+				      }
+				    Target_List_Service targetListSvc = new Target_List_Service();
+				    Target_ListVO trgListVO = targetListSvc.findByTargetID(tagID);
+						
+					user_specVO user = trgListVO.getUserVO();
+		  		
+		  			Target_specVO TargetspecVO=new Target_specVO();
+
+		  			TargetVO TargetVO=new TargetVO();
+
+		  		    TargetSpecService Target_specSvc = new TargetSpecService();
+		  		    
+
+		  		   
+		  		   long time=System.currentTimeMillis();
+		  		    
+		  			Integer UserID = user.getUserID();
+		  				
+		  			
+		  			
+		  	        String trgPicPath="/images/trgSpecs/"+UserID.toString()+targetID+time+".jpg";
+		  	        
+		  	        String Path=request.getServletContext().getRealPath(trgPicPath);
+		  	       
+		  	        
+		  	        File file1=new File(Path);
+		  	        @SuppressWarnings("resource")
+					FileOutputStream fos1 = new FileOutputStream(file1);
+		  	        
+		  	 				
+		  	 				Blob Pic = new SerialBlob(photos);
+		  	 				Pic.getBytes(1, (int) Pic.length());
+		  	 				fos1.write(Pic.getBytes(1, (int) Pic.length()),0,(int) Pic.length());
+		  	 		trgPicPath = "/lifeeditor" + trgPicPath;
+		  	 		Target_specSvc.addTargetSpec(UserID,tagID,note,trgPicPath);
+		 
+		  		} catch (Exception e) {
+		  			e.getStackTrace();
+		  			
+		  		}
+		     
 		    }  
 	}
 
